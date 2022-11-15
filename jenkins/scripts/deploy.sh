4cc1d76e5d315eb2e5d848632e2cdcfb8c1c647a
Suhaili Suri <m.suhaili.suri@gmail.com>
2022-11-15 14:48:09 +0800

    update path

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /Users/suhaili/documents/github/jenkins-php-selenium-test-1\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /Users/suhaili/Documents/GitHub/jenkins-php-selenium-test-1\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
